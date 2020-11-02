814d1fef9bad57d54ed81ac7a9cae80b983d2a22
minisal <1524552292@qq.com>
2020-11-02 10:18:11 +0800

    remove the build result

---
+++ build/CMakeFiles/icp1_simple.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/minisal/desktop/ICP-minisal/src/icp1_simple.cpp" "/Users/minisal/desktop/ICP-minisal/build/CMakeFiles/icp1_simple.dir/src/icp1_simple.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DISABLE_LIBUSB_1_0"
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  "qh_QHpointer"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL2)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL2)"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/vtk@8.2/8.2.0_1/include/vtk-8.2"
+  "/usr/local/include"
+  "/usr/local/include/pcl-1.11"
+  "/usr/local/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/icp1_simple.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/icp1_simple.dir/src/icp1_simple.cpp.o"
+  "icp1_simple"
+  "icp1_simple.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/icp1_simple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
